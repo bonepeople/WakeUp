4d76fa65a3ac23b2816779a2cc688deb37ff8303
bonepeople <bonepeople@sina.com>
2016-03-27 16:06:57 +0800

    添加主界面
    添加HomeActivity，创建子线程方便以后进行扩展

--- src/com/bonepeople/wakeup/SplashActivity.java
+++ src/com/bonepeople/wakeup/SplashActivity.java
@@ -3,17 +3,38 @@ package com.bonepeople.wakeup;
 import com.bonepeople.wakeup.utils.AppInfoUtils;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.AlphaAnimation;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SplashActivity extends Activity
 {
+	public static final int INIT_PASS = 1;
+	public static final int INIT_UPDATE = 2;
+
 	private TextView textView_splash_version;
 	private RelativeLayout relativeLayout_splash_root;
 
-	@Override
+	private Handler handler = new Handler()
+	{
+		public void handleMessage(android.os.Message msg)
+		{
+			switch (msg.what)
+			{
+			case INIT_PASS:
+				break;
+			case INIT_UPDATE:
+				Toast.makeText(getApplicationContext(), "版本过时，需要更新。", Toast.LENGTH_SHORT).show();
+			}
+			Toast.makeText(getApplicationContext(), "后台用时：" + String.valueOf(msg.arg1), Toast.LENGTH_SHORT).show();
+			load_HomeActivity();
+		};
+	};
+
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
@@ -25,6 +46,16 @@ public class SplashActivity extends Activity
 		AlphaAnimation aa = new AlphaAnimation(0.0f, 1.0f);
 		aa.setDuration(2000);
 		relativeLayout_splash_root.startAnimation(aa);
+
+		Thread_init thread_init = new Thread_init(handler);
+		thread_init.start();
+
 	}
 
+	private void load_HomeActivity()
+	{
+		Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+		startActivity(intent);
+		SplashActivity.this.finish();
+	}
 }
---
+++ src/com/bonepeople/wakeup/Thread_init.java
@@ -0,0 +1,42 @@
+package com.bonepeople.wakeup;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class Thread_init extends Thread
+{
+	private Handler handler;
+
+	Thread_init(Handler handler)
+	{
+		this.handler = handler;
+	}
+
+	public void run()
+	{
+		long startTime = System.currentTimeMillis();
+
+		Message msg = Message.obtain();
+
+		msg.what = SplashActivity.INIT_PASS;
+		
+		long endTime = System.currentTimeMillis();
+		long dtime = endTime - startTime;
+
+		if (dtime < 2000)
+		{
+			try
+			{
+				Thread.sleep(2000 - dtime);
+			}
+			catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		msg.arg1 = (int) dtime;
+		handler.sendMessage(msg);
+	}
+
+}
