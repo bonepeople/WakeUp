4029ed18a6e1fbe9bdbf16ccde0392cc314c63eb
bonepeople <bonepeople@sina.com>
2016-07-12 11:00:08 +0800

    添加遍历网络信息的函数
    将本机所有网卡信息打印出来

--- src/com/bonepeople/wakeup/AdbActivity.java
+++ src/com/bonepeople/wakeup/AdbActivity.java
@@ -1,22 +1,19 @@
 package com.bonepeople.wakeup;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-
-import com.bonepeople.wakeup.utils.ShellUtils;
-import com.bonepeople.wakeup.utils.ShellUtils.CommandResult;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class AdbActivity extends Activity implements View.OnClickListener
 {
@@ -35,16 +32,82 @@ public class AdbActivity extends Activity implements View.OnClickListener
 		_button_open.setOnClickListener(this);
 	}
 
+	public String getLocalIpAddress()
+	{
+		try
+		{
+			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();)
+			{
+				NetworkInterface intf = en.nextElement();
+				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();)
+				{
+					InetAddress inetAddress = enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress())
+					{
+						return inetAddress.getHostAddress().toString();
+					}
+				}
+			}
+		}
+		catch (SocketException ex)
+		{
+			Log.e("adb activity", ex.toString());
+		}
+		return null;
+	}
+
+	public static void printParameter(NetworkInterface ni) throws Exception
+	{
+		System.out.println(" Name = " + ni.getName());
+		System.out.println(" Display Name = " + ni.getDisplayName());
+		System.out.println(" Is up = " + ni.isUp());
+		System.out.println(" Support multicast = " + ni.supportsMulticast());
+		System.out.println(" Is loopback = " + ni.isLoopback());
+		System.out.println(" Is virtual = " + ni.isVirtual());
+		System.out.println(" Is point to point = " + ni.isPointToPoint());
+		System.out.println(" Hardware address = " + ni.getHardwareAddress());
+		System.out.println(" MTU = " + ni.getMTU());
+
+		System.out.println("================ List of Interface Addresses:");
+		List<InterfaceAddress> list = ni.getInterfaceAddresses();
+		Iterator<InterfaceAddress> it = list.iterator();
+
+		while (it.hasNext())
+		{
+			InterfaceAddress ia = it.next();
+			System.out.println(" Address = " + ia.getAddress());
+			System.out.println(" Broadcast = " + ia.getBroadcast());
+			System.out.println(" Network prefix length = " + ia.getNetworkPrefixLength());
+			System.out.println("");
+		}
+		System.out.println("================ List over");
+	}
+
 	@Override
 	public void onClick(View v)
 	{
-		Toast.makeText(this, "open", Toast.LENGTH_SHORT).show();
-		System.out.println("send su");
+		// Toast.makeText(this, "open", Toast.LENGTH_SHORT).show();
+		// System.out.println("send su");
+		//
+		// CommandResult _result = ShellUtils.execCommand("setprop service.adb.tcp.port 5555", true, true);
+		// System.out.println("result:" + _result.result);
+		// System.out.println("success:" + _result.successMsg);
+		// System.out.println("error:" + _result.errorMsg);
+		// System.out.println("over");
 
-		CommandResult _result = ShellUtils.execCommand("setprop service.adb.tcp.port 5555", true, true);
-		System.out.println("result:" + _result.result);
-		System.out.println("success:" + _result.successMsg);
-		System.out.println("error:" + _result.errorMsg);
-		System.out.println("over");
+		try
+		{
+			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();)
+			{
+				NetworkInterface intf = en.nextElement();
+				printParameter(intf);
+			}
+		}
+		catch (Exception ex)
+		{
+			Log.e("adb activity", ex.toString());
+		}
+
+		_text_ip.setText(getLocalIpAddress());
 	}
 }
